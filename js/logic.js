bcf16eca636f9b6ee7b38868aa770905dc21a4ce
setti84 <sebastian.settgast@gmail.com>
2018-10-13 16:53:44 +0200

    setting up the scene

---
+++ js/logic.js
@@ -0,0 +1,8 @@
+
+const camera = document.querySelector('#camera');
+const text = document.querySelector('#text');
+
+//text.setAttribute('value', 'Hallo');
+text.setAttribute('text', 'text: Hi');
+
+console.log(text)
